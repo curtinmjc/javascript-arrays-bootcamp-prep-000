e3cf44a13f9d176a0cb66f5bcfe091590f70c9c5
Mike Curtin <curtinmjc@gmail.com>
2018-09-07 17:01:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
 //  const newArray = array.unshift(element);
 //  return newArray;
-  
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(anArray, element) {
