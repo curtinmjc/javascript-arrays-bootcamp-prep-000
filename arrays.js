80e9b6c49dfd3fba4dc1cd780bb1c2411d4a613c
Mike Curtin <curtinmjc@gmail.com>
2018-09-07 16:48:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(array, element) {
 //  const newArray = array.unshift(element);
 //  return newArray;
 //  return;
-  var newArray = array.unshift(element);
+  const newArray = array.unshift(element);
   return newArray;
 }
 
