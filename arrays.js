b52ad8e9911d832ebf3521d097106aeeff845faf
Mike Curtin <curtinmjc@gmail.com>
2018-09-07 16:21:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
+function addElementToBeginningOfArray(anArray, element) {
+  anArray.unshift(element);
+  return anArray
+}
 
-
+function name(anArray, element) {
+  var newArray = anArray.unshift(element);
+  return newArray
+}
